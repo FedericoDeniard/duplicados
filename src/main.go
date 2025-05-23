9def96686a6caac2110507165281c58be7a82e71
Federico <fededeniard@gmail.com>
2025-05-23 13:56:46 -0300

    excluded files

--- src/main.go
+++ src/main.go
@@ -3,20 +3,30 @@ package main
 import (
 	"bytes"
 	"duplicate-files/src/hashes"
+	"flag"
 	"fmt"
 	"os"
 	"os/exec"
+	"strings"
 	"time"
 )
 
 func main() {
 	start := time.Now()
+	var excludedRoutes []string
+	excludedRoutesFlag := flag.String("exclude", "", "Rutas a excluir separadas por comas")
+	flag.Parse()
+	if *excludedRoutesFlag != "" {
+		excludedRoutes = strings.Split(*excludedRoutesFlag, ",")
+	}
+
 	fmt.Printf("\033[1;33mIniciando búsqueda de duplicados\033[0m\n")
 	defer func() {
 		fmt.Printf("\033[1;32mTiempo total: %v\033[0m\n", time.Since(start))
 	}()
+
 	basePath, _ := os.Getwd()
-	filesHashes := hashes.HashFiles(basePath)
+	filesHashes := hashes.HashFiles(basePath, excludedRoutes)
 
 	classified := hashes.GroupByHashes(filesHashes)
 	var output string
